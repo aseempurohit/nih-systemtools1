852c826c893071041a9c9f6aecf244b5c871ef6c
aseempurohit <purohitaseem@gmail.com>
2018-01-15 09:57:43 +0530

    Update system-tools.py

--- system-tools.py
+++ system-tools.py
@@ -14,7 +14,7 @@ redis_db = redis.StrictRedis(host=str(os.environ['REDIS_SERVICE_HOST']), port=st
 
 class SystemToolTime(Resource):
     def get(self):      
-        ms_json = eval(redis_db.get('SYSTEM-TIME-10-1'))
+        ms_json = eval(redis_db.get('SYSTEM-TIME-15-1'))
         url = str(ms_json["10002"])
         headers = {            
             'API-KEY': redis_db.get('API-KEY')
